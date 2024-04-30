68065d3a9dc9d5644233877e4f6f8a5cdcb9a3f7
Rameez <shaikhrameez516@gmail.com>
2024-04-30 13:50:52 +0530

    changed bg color

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212] ">
       <Navbar/>
-    <div className=" mt-24 mx-auto px-12 py-04">
+    <div className=" mt-24 mx-auto px-12 py-04 bg-inherit">
        <HeroSection/>
        <AboutSection/>
        <ProjectsSection/>
